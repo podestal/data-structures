e72150d14f20ae1300409dfa9c3ac431266c541c
podestal <luisrodriguezpodesta@gmail.com>
2023-10-31 11:12:39 -0400

    dll add to head

--- linkedList/doubly_linked_list.py
+++ linkedList/doubly_linked_list.py
@@ -19,3 +19,27 @@ class Node:
   def get_value(self):
     return self.value
     
+class DoublyLinkedList:
+  def __init__(self):
+    self.head_node = Node(None)
+    self.tail_node = Node(None)
+  
+  def add_to_head(self, new_value):
+    new_head = Node(new_value)
+    current_node = self.head_node
+    if current_node != None:
+      current_node.set_prev_node(new_head)
+      new_head.set_next_node(self.head_node)
+    self.head_node = new_head
+    if self.tail_node == None:
+      self.tail_node = new_head
+
+
+dll = DoublyLinkedList()
+dll.add_to_head(15)
+dll.add_to_head(30)
+
+
+
+  
+    
