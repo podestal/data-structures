90d37781c62b788a86396ed3ec4d6ba81fe5d784
podestal <luisrodriguezpodesta@gmail.com>
2023-10-31 11:36:24 -0400

    add to tail

--- linkedList/doubly_linked_list.py
+++ linkedList/doubly_linked_list.py
@@ -34,10 +34,32 @@ class DoublyLinkedList:
     if self.tail_node == None:
       self.tail_node = new_head
 
+  def add_to_tail(self, new_value):
+    pass
+    # create a new_tail node with the new value
+    new_tail = Node(new_value)
+    # create a current_node var which is equal to list's tail_node
+    current_node = self.tail_node
+    # If current_node is not None:
+    if current_node != None:
+    #  - new_tail prev to current_node
+      new_tail.set_prev_node(current_node)
+    #  - current_node next to new_tail
+      current_node.set_next_node(new_tail)
+    # list's tail_node should be equals to new_tail
+    self.tail_node = new_tail
+    # if list's head_node is None:
+    if self.head_node == None:
+    #  - list's head_node equals to new_tail
+      self.head_node = new_tail
 
 dll = DoublyLinkedList()
-dll.add_to_head(15)
-dll.add_to_head(30)
+dll.add_to_tail(2)
+print(dll.tail_node.get_value())
+dll.add_to_tail(6)
+print(dll.tail_node.get_value())
+# dll.add_to_head(15)
+# dll.add_to_head(30)
 
 
 
