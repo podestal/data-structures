35ca6b6e4c61fbe7fa2ab8d73deb9d70bd68765a
podestal <luisrodriguezpodesta@gmail.com>
2023-10-31 13:04:18 -0400

    dll remove tail

--- linkedList/doubly_linked_list.py
+++ linkedList/doubly_linked_list.py
@@ -258,9 +258,36 @@ class LinkedList():
     #  - list's head_node equals to new_tail
       self.head_node = new_tail
 
+  def remove_head(self):
+    # create a var removed_head which is the current head of the list
+    removed_head = self.head_node
+    # if current_head == None:
+    if removed_head == None:
+    #  - return None
+      return None
+    # set list's head_node equals to removed_head's next node
+    self.head_node = removed_head.get_next_node()
+    # If lists head_node is not None
+    if self.head_node != None:
+    #  - Set list's head node's prev node to None
+        self.head_node.set_prev_node(None)
+    # If removed+_head is equal to list's tail node
+    if removed_head == self.tail_node:
+    #  - use remove_tail
+      self.remove_tail() 
+    # return the removed_head
+    return removed_head
+  
+  def remove_tail(self):
+    pass
+
 dll = DoublyLinkedList()
-dll.add_to_head(15)
-dll.add_to_head(30)
+dll.add_to_tail(2)
+print(dll.tail_node.get_value())
+dll.add_to_tail(6)
+print(dll.tail_node.get_value())
+# dll.add_to_head(15)
+# dll.add_to_head(30)
 
 
 
